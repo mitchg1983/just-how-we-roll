f3038e4859fe8dce962e27c870e4c8de9032402c
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-10 20:54:58 -0400

    Add working mean code

--- main.js
+++ main.js
@@ -55,6 +55,36 @@ const twentyDie = document.querySelector('#d20-roll');
 const resetButton = document.querySelector('#reset-button');
 
 
+const d6Mean = document.querySelector('#d6-rolls-mean')
+// const d6Median;
+// const d6Mode;
+
+
+const double6Mean = document.querySelector('#double-d6-rolls-mean');
+
+
+
+const d12Mean = document.querySelector('#d12-rolls-mean');
+
+
+const d20Mean = document.querySelector('#d20-rolls-mean');
+
+
+
+
+
+sixDie.src = 'images/start/d6.png'
+
+doubleSixA.src = 'images/start/d6.png'
+
+doubleSixB.src = 'images/start/d6.png'
+
+twelveDie.src = 'images/start/d12.jpeg'
+
+twentyDie.src = 'images/start/d20.jpg'
+
+
+
 
 
 /*******************
@@ -84,11 +114,32 @@ resetButton.addEventListener('click', reset);
 
 
 function reset () {
-
+  
+  
+  //Splice out the contents of our arrays to reset the data.
   sixes.splice(0, sixes.length);
+  
   doubleSixes.splice(0, doubleSixes.length);
+  
   twelves.splice(0, twelves.length);
+  
   twenties.splice(0, twenties.length);
+  
+  
+  //Reset the images to their starter position.
+  sixDie.src = 'images/start/d6.png'
+  
+  doubleSixA.src = 'images/start/d6.png'
+  
+  doubleSixB.src = 'images/start/d6.png'
+  
+  twelveDie.src = 'images/start/d12.jpeg'
+  
+  twentyDie.src = 'images/start/d20.jpg'
+  
+  
+  //Update the mean, median and mode results.
+  d6Mean.innerText = '';
 
 }
 
@@ -104,15 +155,26 @@ function reset () {
 
 function rollSix() {
   
+  //This section gets the roll, console logs it, and pushes it to our array.
+  
   result = getRandomNumber(6);
   console.log('Your roll of the 6 die is,', result, '.png');
-
+  
   sixes.push(result);
   console.log(sixes);
-
+  
+  
+  //Change the die image to match the roll.
+  
   const newImg = 'images/d6/' + result + '.png';
-
+  
   sixDie.src = newImg;
+  
+  
+  //Update the mean, median and mode results.
+  d6Mean.innerText = (mean(sixes)).toFixed(2);
+  
+  
   
 }
 
@@ -120,19 +182,28 @@ function rollSix() {
 
 function rollDoubleSix() {
   
+  //This section gets the roll, console logs it, and pushes it to our array.
+  
   resultA = getRandomNumber(6);
   resultB = getRandomNumber(6);
-
+  
   console.log('Your roll of the double 6 die is,', resultA, ' ', resultB);
-
+  
   doubleSixes.push(resultA, resultB);
   console.log(doubleSixes);
-
+  
+  
+  //Change the die image to match the roll.
+  
   const newImgA = 'images/d6/' + resultA + '.png';
   const newImgB = 'images/d6/' + resultB + '.png';
-
+  
   doubleSixA.src = newImgA;
   doubleSixB.src = newImgB;
+  
+  
+  //Update the mean, median and mode results.
+  double6Mean.innerText = (mean(doubleSixes)).toFixed(2);
   
 }
 
@@ -140,15 +211,24 @@ function rollDoubleSix() {
 
 function rollTwelve() {
   
+  //This section gets the roll, console logs it, and pushes it to our array.
+  
   result = getRandomNumber(12);
   console.log('Your roll of the 12 die is,', result);
-
+  
   twelves.push(result);
   console.log(twelves);
-
+  
+  
+  //Change the die image to match the roll.
+  
   const newImg = 'images/numbers/' + result + '.png';
-
+  
   twelveDie.src = newImg;
+  
+  
+  //Update the mean, median and mode results.
+  d12Mean.innerText = (mean(twelves)).toFixed(2);
   
 }
 
@@ -156,16 +236,25 @@ function rollTwelve() {
 
 function rollTwenty() {
   
+  //This section gets the roll, console logs it, and pushes it to our array.
+  
   result = getRandomNumber(20);
   console.log('Your roll of the 20 die is,', result);
-
+  
   twenties.push(result);
   console.log(twenties);
-
+  
+  
+  //Change the die image to match the roll.
+  
   const newImg = 'images/numbers/' + result + '.png';
-
+  
   twentyDie.src = newImg;
-
+  
+  
+  //Update the mean, median and mode results.
+  d20Mean.innerText = (mean(twenties)).toFixed(2);
+  
 }
 
 
@@ -173,3 +262,19 @@ function rollTwenty() {
 /****************
 * MATH SECTION *
 ****************/
+
+
+function mean(arr) {
+  
+  let total = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+    )
+    
+    let mean = total / arr.length;
+    
+    return mean
+    
+    
+    
+  }
