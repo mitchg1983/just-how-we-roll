2462e02075b8468aa1cd2b79d0d4fc27699e8c77
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-10 20:15:41 -0400

    Add working die rolls

--- main.js
+++ main.js
@@ -2,14 +2,20 @@
 * DATA *
 **********/
 
+
+
 const sixes = [];
 const doubleSixes = [];
 const twelves = [];
 const twenties = [];
 
+
+
 /********************
 * HELPER FUNCTIONS *
 ********************/
+
+
 
 const getRandomNumber = function(max) {
   const rand = Math.random();
@@ -17,6 +23,8 @@ const getRandomNumber = function(max) {
   const result = Math.ceil(range);
   return result;
 }
+
+
 
 const sortByNumber = function(arr) {
   const byNumber = function(item1, item2) {
@@ -26,28 +34,46 @@ const sortByNumber = function(arr) {
   return arr.slice().sort(byNumber);
 }
 
+
+
 /*******************
 * YOUR CODE BELOW *
 *******************/
 
-function rollSix() {
-  
-  result = getRandomNumber(6);
-  console.log('Your roll of the 6 die is,', result);
 
 
-  
-  
-}
+const sixDie = document.querySelector('#d6-roll');
+
+const doubleSixA = document.querySelector('#double-d6-roll-1');
+
+const doubleSixB = document.querySelector('#double-d6-roll-2');
+
+const twelveDie = document.querySelector('#d12-roll');
+
+const twentyDie = document.querySelector('#d20-roll');
+
+const resetButton = document.querySelector('#reset-button');
 
 
 
-rollSix();
 
 /*******************
 * EVENT LISTENERS *
 *******************/
 
+
+
+sixDie.addEventListener('click', rollSix);
+
+doubleSixA.addEventListener('click', rollDoubleSix);
+
+doubleSixB.addEventListener('click', rollDoubleSix);
+
+twelveDie.addEventListener('click', rollTwelve);
+
+twentyDie.addEventListener('click', rollTwenty);
+
+resetButton.addEventListener('click', reset);
 
 
 
@@ -57,9 +83,90 @@ rollSix();
 
 
 
+function reset () {
+
+  sixes.splice(0, sixes.length);
+  doubleSixes.splice(0, doubleSixes.length);
+  twelves.splice(0, twelves.length);
+  twenties.splice(0, twenties.length);
+
+}
+
+
+
+
+
 /****************************
 * CLICK HANDLING FUNCTIONS *
 ****************************/
+
+
+
+function rollSix() {
+  
+  result = getRandomNumber(6);
+  console.log('Your roll of the 6 die is,', result, '.png');
+
+  sixes.push(result);
+  console.log(sixes);
+
+  const newImg = 'images/d6/' + result + '.png';
+
+  sixDie.src = newImg;
+  
+}
+
+
+
+function rollDoubleSix() {
+  
+  resultA = getRandomNumber(6);
+  resultB = getRandomNumber(6);
+
+  console.log('Your roll of the double 6 die is,', resultA, ' ', resultB);
+
+  doubleSixes.push(resultA, resultB);
+  console.log(doubleSixes);
+
+  const newImgA = 'images/d6/' + resultA + '.png';
+  const newImgB = 'images/d6/' + resultB + '.png';
+
+  doubleSixA.src = newImgA;
+  doubleSixB.src = newImgB;
+  
+}
+
+
+
+function rollTwelve() {
+  
+  result = getRandomNumber(12);
+  console.log('Your roll of the 12 die is,', result);
+
+  twelves.push(result);
+  console.log(twelves);
+
+  const newImg = 'images/numbers/' + result + '.png';
+
+  twelveDie.src = newImg;
+  
+}
+
+
+
+function rollTwenty() {
+  
+  result = getRandomNumber(20);
+  console.log('Your roll of the 20 die is,', result);
+
+  twenties.push(result);
+  console.log(twenties);
+
+  const newImg = 'images/numbers/' + result + '.png';
+
+  twentyDie.src = newImg;
+
+}
 
 
 
